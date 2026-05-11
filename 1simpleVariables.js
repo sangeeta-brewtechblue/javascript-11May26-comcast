32458418bfeb073cb3075f5e0c4b17e2bc512be6
sangeeta-brewtechblue <sangeeta.joshi@brewtechblue.com>
2026-05-11 13:39:27 +0530

    day 1

---
+++ 1simpleVariables.js
@@ -0,0 +1,16 @@
+console.log("Hello world");
+
+var x = "abc"
+
+console.log(x);
+console.log(typeof x)
+
+x = true;
+
+console.log(x);
+console.log(typeof x)
+
+var numArray = [2,6,1,7];
+
+console.log(typeof numArray)
+
